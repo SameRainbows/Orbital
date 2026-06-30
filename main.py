c380c55b41d33e6bc86270377461887d8d17cd55
SameRainbows <avveralabs@gmail.com>
2026-06-30 13:43:26 +0300

    Implement camera panning with WASD and dynamic screen size slingshot spawning

--- main.py
+++ main.py
@@ -6,28 +6,30 @@ t.screen.setup(700,700)
 screen = t.screen
 objs = []
 
+cam_x = 0
+cam_y = 0
+
+
 t.shape("circle")
 t.shapesize(4,4)
 t.speed(0) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
 t.pu()
 
-objs.append([t, 100, 0, 0])
+objs.append([t, 100, 0, 0, 0, 0])
 
 
 
 G = 1000
-def distance(t1, t2): # t1.distance(t2)
+def distance(obj1, obj2): # r = √((x₂−x₁)² + (y₂−y₁)²)
     
-    return t1.distance(t2)
+    return math.sqrt((obj2[4]-obj1[4]) ** 2 + (obj2[5] - obj1[5]) ** 2)
 
 def force(obj1, obj2): # F = G × (m₁ × m₂) / r²
-    force = G * (obj1[1] * obj2[1]) / distance(obj1[0], obj2[0]) ** 2
+    force = G * (obj1[1] * obj2[1]) / distance(obj1,obj2) ** 2
     return force
 
 def direction(obj1,obj2): # θ = atan2(y₂−y₁, x₂−x₁)
-    t1 = obj1[0]
-    t2 = obj2[0]
-    angle = math.atan2(t2.ycor() - t1.ycor(), t2.xcor() - t1.xcor())
+    angle = math.atan2(obj2[5] - obj1[5] , obj2[4]- obj1[4])
     return angle
 
 def velo_update(obj, force, angle, mass):    # Fx = F × cos(θ),  Fy = F × sin(θ)
@@ -55,27 +57,14 @@ def move():
         vx = body[2]
         vy = body[3]
 
-        x = body[0].xcor()
-        y = body[0].ycor()
-
-        body[0].goto(x + vx, y + vy)
-
+        x = body[4]
+        y = body[5]
+        body[4] += vx
+        body[5] += vy
+        body[0].goto(body[4] - cam_x, body[5] - cam_y)
+        
     screen.ontimer(move, t=1)
-    """obj1 = objs[0]
-    obj2 = objs[1]
-
-    vx = obj2[2]
-    vy = obj2[3]
-
-    F = force(obj1, obj2)
-    D = direction(obj2, obj1)
-    velo_update(objs[1], F, D, obj2[1])
-
-    x = obj2[0].xcor()
-    y = obj2[0].ycor()
     
-    obj2[0].goto(x + vx, y + vy)
-    screen.ontimer(move, t=1)"""
 
 canvas = screen.getcanvas()
 press_x = None
@@ -87,14 +76,16 @@ def sling(event):
     global sling_state
     global press_x
     global press_y
+    scww = canvas.winfo_width() / 2
+    scwh = canvas.winfo_height() / 2
 
     if sling_state == None:
-        press_x = event.x - 350 
-        press_y = 350 - event.y
+        press_x = (event.x - scww) + cam_x
+        press_y = (scwh - event.y) + cam_y
         sling_state = press_x, press_y
     else:
-        release_x = event.x - 350 
-        release_y = 350 - event.y
+        release_x = (event.x - scww) + cam_x
+        release_y = (scwh - event.y) + cam_y
         sling_state = None
         vx = press_x - release_x
         vy = press_y - release_y
@@ -104,17 +95,32 @@ def sling(event):
         new_body.shape('circle')
         new_body.speed(0)
         new_body.penup()
-        new_body.goto(press_x, press_y)
-        objs.append([new_body, 1, vx, vy])
+        new_body.goto(press_x - cam_x, press_y - cam_y)
+        objs.append([new_body, 1, vx, vy, press_x, press_y])
         
-
+def w():
+    global cam_y
+    cam_y += 10
+def a():
+    global cam_x
+    cam_x -= 10
+def s():
+    global cam_y
+    cam_y -= 10
+def d():
+    global cam_x
+    cam_x += 10
     
 
 canvas.bind("<ButtonPress-1>", sling)
 canvas.bind("<ButtonRelease-1>", sling)
 
+screen.onkeypress(w, "w")
+screen.onkeypress(a, "a")
+screen.onkeypress(s, "s")
+screen.onkeypress(d, "d")
 
-
+screen.listen()
 
 
 move()
